4413d4e68ed185ae473bd41a6c59f6cefd9c170d
kawthrham <116515282+kawthrham@users.noreply.github.com>
2022-10-27 19:12:14 +0400

    change welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,8 @@
         </style>
     </head>
     <body class="antialiased">
+    <h1> Welcome! My name is: Maryam.This is the Staging Environment </h1>
+    /*
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
@@ -127,6 +129,6 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div>*/
     </body>
 </html>
